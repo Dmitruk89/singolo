acae26b2d577565d76263db0b8c5ab89dd39bb36
Dmitry Rukovichnikov <dmitry.rukovichnikov@gmail.com>
2020-03-16 20:44:56 +0300

    refactor: section slider plus DOM

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// navigation
+
 document.querySelector('.header__navigation').addEventListener('click', selectNavigationLink);
 
 function selectNavigationLink(event) {
@@ -8,4 +10,31 @@ function selectNavigationLink(event) {
     if (event.target.tagName === "A") {
         event.target.parentElement.classList.add('active');
     }
+}
+
+// slider
+
+document.querySelectorAll(".phone__btn").forEach( element => {
+    element.addEventListener('click', () => {
+        let phoneScreen = element.parentElement.querySelector('.phone__screen');
+        if (phoneScreen.style.opacity === "0"){
+            phoneScreen.style.opacity = "1";
+        } else {
+            phoneScreen.style.opacity = "0";
+        }
+    });
+});
+
+// portfolio
+
+document.querySelector(".portfolio__gallery").addEventListener('click', selectGalleryItem);
+
+function selectGalleryItem(event) {
+    if(event.target.parentElement.classList.contains('gallery__item')) {
+        this.querySelectorAll('.selected').forEach( element => {
+            element.classList.remove('selected');
+        });
+        event.target.classList.add('selected');
+    } 
+    
 }

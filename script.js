c968ecb17175a7a8d34b35a2cf7082356da1f267
Dmitry Rukovichnikov <dmitry.rukovichnikov@gmail.com>
2020-03-21 12:35:08 +0300

    refactor: complete dom task at navigation portfolio and form

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 // navigation
 
-document.querySelector('.header__navigation').addEventListener('click', selectNavigationLink);
+document.addEventListener('scroll', activeOnScroll);
 
-function selectNavigationLink(event) {
-    document.querySelectorAll('.navigation__link').forEach( element => {
-        element.classList.remove('active');
-    });
+function activeOnScroll(event){
+    let navLink = document.querySelectorAll('.navigation__link a');
+    let currentPosition = window.scrollY;
+    let sections = document.querySelectorAll('section');
 
-    if (event.target.tagName === "A") {
-        event.target.parentElement.classList.add('active');
-    }
-}
+     sections.forEach((el) => {
+         if(el.offsetTop - 100 <= currentPosition && (el.offsetTop + el.offsetHeight - 100) > currentPosition){
+             navLink.forEach((a) => {
+                 a.classList.remove('active');
+                 if(el.firstElementChild.getAttribute('name') === a.getAttribute('href').substring(1)){
+                    a.classList.add('active');
+                 };
+             })
+         }
+     });
+};
+
 
 // slider
 
@@ -35,6 +43,47 @@ function selectGalleryItem(event) {
             element.classList.remove('selected');
         });
         event.target.classList.add('selected');
-    } 
     
+    }
+
+}
+
+document.querySelectorAll('.tab').forEach((element) => {
+    element.addEventListener('click', tabSelection);
+});
+
+
+function tabSelection(event) {
+    
+    document.querySelectorAll('.tab').forEach( element => {
+        element.classList.remove('tab_selected');  
+    });
+
+    event.target.classList.add('tab_selected');
+    
+}
+
+// get a quote
+
+document.getElementById('modal-btn').addEventListener('click', hideModalWindow);
+
+function hideModalWindow(event){
+    document.querySelector('.modal').style.display = 'none';
+}
+
+document.querySelector('.get-a-quote__form').addEventListener('submit', submitForm);
+
+function submitForm(event){
+    event.preventDefault();
+
+    let subject = document.getElementById('subject').value;
+    let description = document.getElementById('description').value;
+
+    document.querySelector('.modal').style.display = 'flex';
+
+    document.querySelector('.modal__text').innerHTML = `The letter was sent <br>
+    ${!!subject ? 'Subject: ' + subject : 'Without subject'}<br>
+    ${!!description ? 'Description: ' + description : 'Without description'}`;
+
+
 }
